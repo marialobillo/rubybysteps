a1edda514df388270ff74fbda75463fbd98e7b41
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-16 11:30:53 +0200

    exercise 2

--- e1.rb
+++ e1.rb
@@ -2,3 +2,13 @@ puts "Hello World"
 puts Time.now
 puts Array.new([1,2,3])
 puts rand(128).class
+
+class MyClass
+  def to_s
+    "I am a MyClass"
+  end
+end
+
+puts MyClass.new
+
+puts "Hello", "testing", "puts", 12345
---
+++ e2.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+name = gets
+puts "What is your quest?"
+quest = gets
+puts "What is yout favorite color?"
+color = gets
+
+puts "Your name is"
+puts name
+puts "Your quest is"
+puts quest
+puts "Your favorite color is"
+puts color
