c70bcd0db37be11f9e625ecd7c1ed82835fc1620
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-17 11:14:22 +0200

    tiny test 1

---
+++ tinytests/e1.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+
+class ArrayTest < MiniTest::Test
+  def test_new_array_has_zero_size
+    assert(Array.new.size == 0)
+  end
+
+  def test_new_array_is_empty
+    assert(Array.new.empty?)
+  end
+
+  def test_empty_array_is_equivalent
+    assert(Array.new == [])
+  end
+
+  def test_initialized_array_is_equivalent
+    assert(Array.new([1,2,3]) == [1,2,3])
+  end
+
+  def test_pop_the_top
+    asser(Array.new([1,2,3]).pop == 3)
+  end
+end
