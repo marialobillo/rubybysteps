a92981c78d3a3d521981e3f0959c1c606808f53d
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-18 18:59:47 +0200

    exercise 1 b

---
+++ messages/e1b.rb
@@ -0,0 +1,45 @@
+class Add10
+  def +(number)
+    10 + number
+  end
+end
+
+p(Add10.new + 5)
+
+class RandomList
+  def [](count)
+    list = []
+    count.times { list << rand(100) }
+    list
+  end
+end
+
+p(RandomList.new[3])
+
+class Summing
+  def initialize(value)
+    @value = value
+  end
+
+  def &(other)
+    self.class.new(@value + other.to_i)
+  end
+
+  def inspect
+    "@value = #{@value}"
+  end
+
+  def to_i
+    @value
+  end
+end
+
+p(Summing.new(1) & 2 & 3)
+
+class Fixnum
+  def +(other)
+    self - other
+  end
+end
+
+p(10 + 5)
