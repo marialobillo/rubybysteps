827c17e28f5b62b1fc397fc6f25ccb988903f698
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-19 12:32:01 +0200

    ref 2: second refactor

--- refactoring/e2.rb
+++ refactoring/e2.rb
@@ -5,9 +5,9 @@ class MyClass
     at_most_users = 5
     registration_threshold = 3.days.ago
 
-    users.select {|u| u.registered? && u.registered_before(registration_threshold) }.
-      reject {|u| u.role == admin_role }.
-      sort_by {|u| referral_streams.index u.referral_stream }.
-      first(at_most_users)
-  end
+    registered_users = users.select {|u| u.registered? && u.registered_before(registration_threshold) }
+    non_admins = registered_users.reject {|u| u.role == admin_role }
+    ordered_users = non_admins.sort_by {|u| referral_streams.index u.referral_stream }
+
+    ordered_users.first(at_most_users)  end
 end
