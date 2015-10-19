c8852089ead09ce8e5984f4b1074f53697fa9d1d
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-19 11:53:16 +0200

    refactoring exercise 1

---
+++ refactoring/e1.rb
@@ -0,0 +1,31 @@
+class Foo
+  def something(bar)
+    bar.do_something
+  end
+end
+
+class Account
+  def initialize
+    @balance = "0"
+  end
+
+  def deposit(amount)
+    amount.times { @balance.succ! }
+  end
+
+  def balance
+    @balance.to_i
+  end
+
+  def downcased_email(username, domain)
+    [username, domain].map(&:downcase).join "@"
+  end
+
+  def copy_lines_matching(infilename, outfilename, regex)
+    lines = File.readlines(File.expand_path(infilename)).select
+    {|l| regex.math l }
+    File.open(File.expand_path(outfilename), 'w') do |file|
+      lines.each {|l| file << l}
+    end
+  end
+end
