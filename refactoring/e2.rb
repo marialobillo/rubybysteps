75e7bfc16c67c21952d447c6d307771dba5f5e39
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-19 12:32:37 +0200

    3th step

--- refactoring/e2.rb
+++ refactoring/e2.rb
@@ -1,13 +1,25 @@
 class MyClass
   def top_users(users)
-    admin_role = "admin"
-    referral_streams = %w(email website twitter)
-    at_most_users = 5
-    registration_threshold = 3.days.ago
-
     registered_users = users.select {|u| u.registered? && u.registered_before(registration_threshold) }
     non_admins = registered_users.reject {|u| u.role == admin_role }
     ordered_users = non_admins.sort_by {|u| referral_streams.index u.referral_stream }
 
-    ordered_users.first(at_most_users)  end
+    ordered_users.first(at_most_users)
+  end
+
+  def registration_threshold
+    3.days.ago
+  end
+
+  def admin_role
+    "admin"
+  end
+
+  def referral_streams
+    %w(email website twitter)
+  end
+
+  def at_most_users
+    5
+  end
 end
