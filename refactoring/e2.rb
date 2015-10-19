ee75ae7df3c3049c14ba34e32ed8f55c0fe3a656
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-19 12:36:59 +0200

    4th step refactor

--- refactoring/e2.rb
+++ refactoring/e2.rb
@@ -1,10 +1,24 @@
 class MyClass
   def top_users(users)
-    registered_users = users.select {|u| u.registered? && u.registered_before(registration_threshold) }
-    non_admins = registered_users.reject {|u| u.role == admin_role }
-    ordered_users = non_admins.sort_by {|u| referral_streams.index u.referral_stream }
+    registered_users = recently_registered users
+    non_admins = all_non_admins registered_users
+    ordered_users = order_by_referral_stream non_admins
 
     ordered_users.first(at_most_users)
+  end
+
+  private
+
+  def recently_registered(users)
+    users.select {|u| u.registered? && u.registered_before(registration_threshold)}
+  end
+
+  def all_non_admins
+    users.reject {|u| u.role == admin_role }
+  end
+
+  def order_by_referral_stream(users)
+    users.sort_by {|u| referral_streams.index u.referral_streams }
   end
 
   def registration_threshold
