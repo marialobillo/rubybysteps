423ff9dfe94c2879bd21081730112f6b26bef009
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-19 12:48:15 +0200

    5th step refactor

--- refactoring/e2.rb
+++ refactoring/e2.rb
@@ -1,25 +1,28 @@
 class MyClass
-  def top_users(users)
-    registered_users = recently_registered users
-    non_admins = all_non_admins registered_users
-    ordered_users = order_by_referral_stream non_admins
 
-    ordered_users.first(at_most_users)
+  class UserList < Array
+    def recently_registered(treshold)
+      self.class.new(select {|u| u.registered? && u.registered_before(treshold)})
+    end
+
+    def all_non_role(role)
+      self.class.new(reject {|u| u.role == role })
+    end
+
+    def order_by_referral_stream(referral_streams)
+      self.class.new(sort_by {|u| referral_streams.index u.referral_stream })
+    end
+  end
+
+
+  def top_users(users)
+    UserList.new(users).recently_registered(registration_threshold).
+      all_non_role(admin_role).
+        order_by_referral_stream(referral_streams).
+          first(at_most_users)
   end
 
   private
-
-  def recently_registered(users)
-    users.select {|u| u.registered? && u.registered_before(registration_threshold)}
-  end
-
-  def all_non_admins
-    users.reject {|u| u.role == admin_role }
-  end
-
-  def order_by_referral_stream(users)
-    users.sort_by {|u| referral_streams.index u.referral_streams }
-  end
 
   def registration_threshold
     3.days.ago
