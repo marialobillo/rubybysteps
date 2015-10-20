1a1cdc200d799ea93aac04a0e09abae726554090
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-20 15:09:24 +0200

    challenges

---
+++ rubyexs/ex1.rb
@@ -0,0 +1,7 @@
+
+ar = [1,2,3,4,5,6]
+first = 0
+last = ar.length - 1
+an = [ ar[first], ar[last]]
+
+print an 
---
+++ rubyexs/ex2.rb
@@ -0,0 +1,13 @@
+grade = 110
+
+
+case grade
+    when 90..100
+      puts "Hooray!"
+    when 80...90
+      puts "OK job"
+    when 70...80
+      puts "You can do better"
+    else
+      puts "You failed!"
+end
