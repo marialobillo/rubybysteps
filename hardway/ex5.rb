5e96b05f0085e4019d47df8722a8a12e1d51d0eb
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-23 16:43:35 +0200

    Hard way: 5

---
+++ hardway/ex5.rb
@@ -0,0 +1,18 @@
+
+my_name = "Maria L."
+my_age = 33
+my_height = 170 # cm
+my_weight = 66 # kilos
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+puts "Let's talk about %s" % my_name
+puts "She's %d centimetres tall." % my_height
+puts "She's %d kilos heavy." % my_weight
+puts "Actually that's not too heavy."
+puts "She's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age + my_height + my_weight]
