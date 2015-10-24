003936a61a317b9faa0cd3ad0fd51889941d3cb1
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-24 17:08:20 +0200

    the hard way: ex 13 with variations

---
+++ hardway/ex13.rb
@@ -0,0 +1,8 @@
+first, second, third, *food = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "The meal es: #{food[0]}"
+puts "Dinner is: #{food[1]}" 
