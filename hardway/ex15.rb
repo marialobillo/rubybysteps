5b209890602d43dd5fbd94659897a1a2fa098558
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-25 11:49:51 +0100

    ex15: files, completed

--- hardway/ex15.rb
+++ hardway/ex15.rb
@@ -1,16 +1,25 @@
 
+# save the file name
 filename = "ex15_sample.txt"
 
+#print the prompt
 prompt = "> "
+# open the file
 txt = File.open(filename)
 
+# print the file name and read the file
 puts "Here's your file: #{filename}"
 puts txt.read()
+txt.close()
 
+# gets the filename again
 puts "I'll also ask you to type it again:"
 print prompt
 file_again = STDIN.gets.chomp()
 
+# open again the file and save in a variable
 txt_again = File.open(file_again)
 
+# print the file variable content
 puts txt_again.read()
+txt_again.close()
