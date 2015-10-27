88219519341d9d267ae606308323536627396852
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-27 10:39:33 +0100

    test finished

--- hardway/ex26.rb
+++ hardway/ex26.rb
@@ -1,52 +1,52 @@
-module Ex2
+module Ex25
 
   # This function will break up words for us.
-  def self.brak_words(stuff
+  def self.break_words(stuff)
     words = stuff.split(' ')
-    return word
+    words
   end
 
   # Sorts the words.
-  def self.sortwords(words)
-    return words.sort
+  def self.sort_words(words)
+    words.sort()
   end
 
   # Prints the first word after popping it off.
   def self.print_first_word(words)
-    word = words.pop(1)
-    puts wor
+    word = words.shift()
+    puts word
   end
 
   # Prints the last word after popping it off.
   def self.print_last_word(words)
-    word = words.pop
-    put word
+    word = words.pop()
+    puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
   def self.sort_sentence(sentence)
-    words = Ex25.break_words(sentence)
-    return Ex25.sort_words(words)
-  ed
+    words = break_words(sentence)
+    return sort_words(words)
+  end
 
   # Prints the first and last words of the sentence.
-  def self.print_first_and_last(sentence
-    words = Ex25.break_words(sentenc)
-    Ex25.print_first_wrd(word)
-    Ex25.print_last_word(words)
+  def self.print_first_and_last(sentence)
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one.
   def self.print_first_and_last_sorted(sentence)
-    words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
-    Ex25.print_last_word(words)
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
   end
 
-
+end
 
 puts "Let's practice everything."
-puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+puts "You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs."
 
 poem = <<END
 \tThe lovely world
@@ -62,32 +62,32 @@ puts poem
 puts "--------------"
 
 
-five = 10 - 2 * 3 - 6
+five = 10 - 2 + 3 - 6
 puts "This should be five: #{five}"
 
 def secret_formula(started)
-  jelly_bens = started * 500
+  jelly_beans = started * 500
   jars = jelly_beans / 1000
-  crate = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
 end
 
 
 start_point = 10000
-beans, jars crates = secret_formula(start_point)
+jelly_beans, jars, crates = secret_formula(start_point)
 
 puts "With a starting point of: #{start_point}"
-puts "We'd have #{beans beans, #{jars} jars, and #{crates} crates."
+puts "We'd have #{jelly_beans} jelly beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point / 10
 
 sentence = "All good things come to those who wait."
 words = Ex25.break_words(sentence)
 sorted_words = Ex25.sort_words(words)
-Ex25.print_first_word(wrds)
-Ex25.print_last_word words)
-Ex25.print_first_word(sort_words)
+Ex25.print_first_word(words)
+Ex25.print_last_word(words)
+Ex25.print_first_word(sorted_words)
 Ex25.print_last_word(sorted_words)
-sorted_words = Ex25.sort_sentenc(sentence)
+sorted_words = Ex25.sort_sentence(sentence)
 Ex25.print_first_and_last(sentence)
-Ex25:print_first_and_last_sorted(sentence)
+Ex25.print_first_and_last_sorted(sentence)
