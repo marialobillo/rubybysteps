16e76733f6e0be959df5a2593993bee29f1604e4
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-29 13:08:13 +0100

    better ex 34

--- hardway/ex34.rb
+++ hardway/ex34.rb
@@ -2,5 +2,5 @@
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 
 animals.each_with_index do |animal, index|
-  puts "The #{index+1} is at #{index} and is a #{animal}"
+  puts "The #{index+1}th is at #{index} and is a #{animal}"
 end
