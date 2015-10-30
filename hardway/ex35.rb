198808eace893945c2da13cfa19b33e91b8c5a0a
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-30 17:17:50 +0100

    ex 35 final

--- hardway/ex35.rb
+++ hardway/ex35.rb
@@ -36,4 +36,48 @@ def bear_room()
       puts "The bear has moved from the door. You can go through it now."
       bear_moved = true
     elsif next_move == "taunt bear" and bear_moved
+      gold_room()
+    else
+      puts "I got no idea what that means."
+    end
+  end
 end
+
+def cthulu_room()
+  puts "Here you see the great evilt Cthulu."
+  puts "He, it, whatever stares at you and you go insane."
+  puts "Do you flee for your life or eat you head?"
+
+  prompt; next_move = gets.chomp
+
+  if next_move.include? "flee"
+    start()
+  elsif next_move.include? "head"
+    dead("Well that was tasty!")
+  else
+    cthulu_room()
+  end
+end
+
+def dead(why)
+  puts "#{why} Good Job!"
+  Process.exit(0)
+end
+
+def start()
+  puts "You are in a dark room."
+  puts "There is a door to your right and left."
+  puts "Which one do you take?"
+
+  prompt; next_move = gets.chomp
+
+  if next_move == "left"
+    bear_room()
+  elsif next_move == "right"
+    cthulu_room()
+  else
+    dead("You stumble around the room until you starve.")
+  end
+end
+
+start()
