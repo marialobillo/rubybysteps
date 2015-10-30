8f348a87e0926dec38c01d659c6568f2ae2962aa
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-30 19:37:59 +0100

    ex 36 final

--- hardway/ex36.rb
+++ hardway/ex36.rb
@@ -12,7 +12,7 @@ def start()
   puts "One to your left and other on your right."
   puts "Wich one do you take?"
 
-  prompt; next_move = get.chomp
+  prompt; next_move = gets.chomp
 
   if next_move == "left"
     wolf_room()
@@ -20,16 +20,46 @@ def start()
     gondor_room()
   else
     gameove('You only hace a few seconds to respond!! Game Over!!')
+  end
 end
 
 def wolf_room()
+
+  puts "-------------------"
+  puts "There is a table and in that table there is a knife."
+  puts "In that room there is a wolf too."
+  puts "And you have 3 kilos of meat."
+  puts "You have to choose between the knife and meat."
+
+  prompt; next_move = gets.chomp
+
+  if next_move == "knife"
+    diamond_room()
+  elsif next_move == "meat"
+    wolf_room()
+  else
+    gameover('You had to choose something!! Game Over!!')
+  end
 
 end
 
 def gondor_room()
 
-end
-
-def ruby_room()
+  gameover('There is too fire in here!! GAME OVER!!')
 
 end
+
+def diamond_room()
+  puts "A lot of Diamonds!! How many you can take?"
+
+  prompt; next_move = gets.chomp
+
+  if next_move.to_i < 10
+    gameover('It is not enough, sorry!!')
+  elsif next_move.to_i >= 10
+    puts "Great!! You win!!"
+    Process.exit(0)
+  end
+end
+
+start()
